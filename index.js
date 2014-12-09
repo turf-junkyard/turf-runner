003f44a848542f8356b0e714c22522160ed1f446
morganherlocker <morgan.herlocker@gmail.com>
2014-12-09 15:48:10 -0500

    git clone all modules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var fs = require('fs')
+var fs = require('fs');
+var exec = require('child_process').exec;
+var rimraf = require('rimraf');
 
-//read all modules from turf
+// read all modules from turf
 var modules = [];
 var index = fs.readFileSync(__dirname + '/node_modules/turf/index.js').toString();
 var regex = /\'[^\']*\'/; 
@@ -11,5 +13,11 @@ index.forEach(function(line){
 	if(module)
 		modules.push(module[0].split('\'').join(''));
 })
-console.log(modules)
-
+rimraf(__dirname+'/submodules', function(){
+	modules.forEach(function(module){
+		// clone the module into the local directory
+		exec('git clone git@github.com:Turfjs/'+module+'.git '+__dirname+'/submodules/'+module, function (err, stdout, stderr){
+			if(err) console.log(err)
+		});
+	});
+})
