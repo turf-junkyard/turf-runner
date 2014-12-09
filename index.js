69f8d87b593a1d2251fe65cbe6c1935075fef919
morganherlocker <morgan.herlocker@gmail.com>
2014-12-09 18:06:04 -0500

    benching

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 var rimraf = require('rimraf');
+var queue = require('queue-async');
 
 // read all modules from turf
 var modules = [];
@@ -10,14 +11,40 @@ index = index.split('\n');
 
 index.forEach(function(line){
 	var module = regex.exec(line);
-	if(module)
-		modules.push(module[0].split('\'').join(''));
+	if(module) modules.push(module[0].split('\'').join(''));
 })
+// remove existing submodules
 rimraf(__dirname+'/submodules', function(){
 	modules.forEach(function(module){
 		// clone the module into the local directory
-		exec('git clone git@github.com:Turfjs/'+module+'.git '+__dirname+'/submodules/'+module, function (err, stdout, stderr){
-			if(err) console.log(err)
-		});
+		exec('git clone git@github.com:Turfjs/'+module+'.git '+__dirname+'/submodules/'+module, function(){
+			benchmarkSetup(module);
+		})
 	});
-})
+});
+
+function benchmarkSetup(module) {
+	var benchString = 
+'var  = require(\'./\')\n'+
+'var benchmark = require(\'Benchmark\')\n'+
+'var fs = require(\'fs\')\n'+
+'\n'+
+'var suite = new Benchmark.Suite(\''+module+'\');\n'+
+'suite\n'+
+'  .add(\'index\',function {\n'+
+'    \n'+
+'   })\n'+
+'   .on(\'cycle\', function(event) {\n'+
+'     console.log(String(event.target));\n'+
+'   })\n'+
+'  .on(\'complete\', function() {\n'+
+'     \n'+
+'   })\n'+
+'   .run();\n'
+
+	fs.writeFileSync(__dirname+'/submodules/'+module+'/bench.js', benchString)
+	exec('(cd '+__dirname+'/submodules/'+module+' && npm install benchmark --save-dev)', function(err){
+		if (err) console.log(err)
+		console.log(module)
+	})
+}
